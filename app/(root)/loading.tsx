27c31bbf1ed92d2448398d2d076cb5dc90c89f54
vasenkom <m.vasenko@yandex.ru>
2025-10-27 17:05:23 +0100

    choir: css UI fixes

--- app/(root)/loading.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-import { Spinner } from "@/components/ui/spinner"
-
-export default function HomeLoading() {
-    return (
-        <Spinner className="size-12" />
-    )
-}
--- app/_lib/components/NavigationMenuHeader.tsx
+++ app/_lib/components/NavigationMenuHeader.tsx
@@ -11,26 +11,30 @@ import Link from "next/link"
 
 export default function NavigationMenuHeader() {
   return (
-    <>
+    <div className="border-b-1 flex flex-row justify-center h-10">
         <NavigationMenu>
             <NavigationMenuList>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <Link href="#">Home</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <Link href="#">CSR</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                    <NavigationMenuLink asChild>
-                        <Link href="#">SSR</Link>
-                    </NavigationMenuLink>
-                </NavigationMenuItem>
+                <div className="flex flex-row justify-between gap-25">
+                    <NavigationMenuItem>
+                        <NavigationMenuLink asChild>
+                            <Link href="/">Home</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    <div className="flex flex-row gap-5">
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild>
+                                <Link href="/CSR">CSR</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuLink asChild>
+                                <Link href="/SSR">SSR</Link>
+                            </NavigationMenuLink>
+                        </NavigationMenuItem>
+                    </div>
+                </div>
             </NavigationMenuList>
         </NavigationMenu>
-    </>
+    </div>
   )
 }

b80f71b04a2795eab7317c1e1b95d284f61efc50
vasenkom <m.vasenko@yandex.ru>
2025-10-27 17:12:05 +0100

    feat: add SSR

---
+++ app/(root)/CSR/loading.tsx
@@ -0,0 +1,8 @@
+'use client'
+import { Spinner } from "@/components/ui/spinner"
+
+export default function Loading() {
+    return (
+        <Spinner className="size-12" />
+    )
+}
---
+++ app/(root)/CSR/page.tsx
@@ -0,0 +1,7 @@
+export default function CRS() {
+    return (
+        <div className="mx-auto w-[80%] mt-4">
+            <h1>CRS Weather Dashboard</h1>
+        </div>
+    )
+}

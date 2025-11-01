a7ebc360d8160a189b59469506c3b8827ea20313
vasenkom <m.vasenko@yandex.ru>
2025-11-01 20:16:38 +0100

    feat: add context, wrap in WeatherProvider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,11 @@
 import NavigationMenuHeader from "@/app/_lib/components/NavigationMenuHeader"
+import { WeatherProvider } from "./SSR/context"
 
 export default function Layout({children} : {children: React.ReactNode}) {
     return (
-        <>
+        <WeatherProvider>
             <NavigationMenuHeader />
             {children}
-        </>         
+        </WeatherProvider>         
     )
 }

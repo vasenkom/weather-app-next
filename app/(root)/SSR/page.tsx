bebadfe03627ba379647ffa39490133d0c147ebf
Marina <145867783+vasenkom@users.noreply.github.com>
2025-11-22 18:46:48 +0100

    wip:  fix to SSR only (#5)

--- app/(root)/SSR/page.tsx
+++ app/(root)/SSR/page.tsx
@@ -1,17 +1,35 @@
-"use client"
-
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { useWeatherContext } from "./context"
 import { fetchWeather } from "@/app/_lib/fetch/fetchWeather"
-import { useState } from "react"
 import Cards from "@/app/_lib/components/Cards"
 
-export default function SSR() {
-    const {updateInputState} = useWeatherContext()
-    const [currentWeather, setCurrentWeather] = useState<any>(null);
-    const [forecastWeather, setForecastWeather] = useState<any>(null);
-    const [error, setError] = useState<string | null>(null);
+type SSRprops = {
+    searchParams?: {
+    city?: string;
+  };
+}
+
+export default async function SSR({searchParams}: SSRprops) {
+    const cityQ = await searchParams // access to URL query parameters
+    const cityQuery = cityQ?.city ?? '';
+    
+    let currentWeather: any = null;
+    let forecastWeather: any = null;
+    let error: string | null = null;
+
+    if (cityQuery) {
+        try {
+           const data = await fetchWeather(cityQuery); 
+            if (data.error) {
+                error = data.error
+            } else {
+               currentWeather = data.current
+               forecastWeather = data.forecast
+            }
+        } catch (e: any) {
+            error = "Failed to fetch weather.";
+        }
+    }
 
     const city = currentWeather?.location?.name || null
     const country = currentWeather?.location?.country || null
@@ -31,32 +49,12 @@ export default function SSR() {
     const futureTempMax = forecastWeather?.forecast?.forecastday?.[2]?.day?.maxtemp_c || null;
     const futureTempMin = forecastWeather?.forecast?.forecastday?.[2]?.day?.mintemp_c || null;
 
-    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        setError(null);
-
-        //FormData allows to read inputs from form; strores key-value pairs
-        const city = (new FormData(e.currentTarget).get("city") as string)
-        updateInputState('city', city) 
-
-        const data = await fetchWeather(city);
-
-        if (data.error) {
-            setError(data.error);
-            setCurrentWeather(null);
-            setForecastWeather(null);
-            return;
-        }
-
-        setCurrentWeather(data.current);
-        setForecastWeather(data.forecast);
-    }
-
     return (
         <div className="mx-auto w-[80%] mt-4">
             <h1>SSR Weather Dashboard</h1>
             <div className="flex flex-col items-start w-full max-w-sm items-center gap-2 mt-4">
-                <form onSubmit={onSubmit} className="flex gap-2">
+                {/* Reload the current page, but add the form fields to the URL */}
+                <form method="GET" className="flex gap-2"> 
                     <Input type="text" placeholder="City" name="city" />
                     <Button type="submit" variant="outline">
                         Get Weather

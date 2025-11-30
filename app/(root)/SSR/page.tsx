45389cedbb700040450330cfce7d1606e0247544
vasenkom <m.vasenko@yandex.ru>
2025-11-30 14:36:43 +0100

    fix: change null to 0; work on mobile responsiveness

--- app/(root)/SSR/page.tsx
+++ app/(root)/SSR/page.tsx
@@ -31,23 +31,23 @@ export default async function SSR({searchParams}: SSRprops) {
         }
     }
 
-    const city = currentWeather?.location?.name || null
-    const country = currentWeather?.location?.country || null
-    const currentTemperature = currentWeather?.current?.temp_c || null
-    const feelsLike = currentWeather?.current?.feelslike_c || null
-    const condition = currentWeather?.current?.condition?.text || null
-    const conditionIcon = currentWeather?.current?.condition?.icon || null
-    const precip_mm = currentWeather?.current?.precip_mm || null
-    const wind_kph = currentWeather?.current?.wind_kph || null
-    const wind_dir = currentWeather?.current?.wind_dir || null
+    const city = currentWeather?.location?.name ?? 0
+    const country = currentWeather?.location?.country ?? 0
+    const currentTemperature = currentWeather?.current?.temp_c ?? 0
+    const feelsLike = currentWeather?.current?.feelslike_c ?? 0
+    const condition = currentWeather?.current?.condition?.text ?? 0
+    const conditionIcon = currentWeather?.current?.condition?.icon ?? 0
+    const precip_mm = currentWeather?.current?.precip_mm ?? 0
+    const wind_kph = currentWeather?.current?.wind_kph ?? 0
+    const wind_dir = currentWeather?.current?.wind_dir ?? 0
 
-    const tomorrowDate = forecastWeather?.forecast?.forecastday?.[1]?.date || null;
-    const tomorrowTempMax = forecastWeather?.forecast?.forecastday?.[1]?.day?.maxtemp_c || null;
-    const tomorrowTempMin = forecastWeather?.forecast?.forecastday?.[1]?.day?.mintemp_c || null;
+    const tomorrowDate = forecastWeather?.forecast?.forecastday?.[1]?.date ?? 0;
+    const tomorrowTempMax = forecastWeather?.forecast?.forecastday?.[1]?.day?.maxtemp_c ?? 0;
+    const tomorrowTempMin = forecastWeather?.forecast?.forecastday?.[1]?.day?.mintemp_c ?? 0;
 
-    const futureDate = forecastWeather?.forecast?.forecastday?.[2]?.date || null;
-    const futureTempMax = forecastWeather?.forecast?.forecastday?.[2]?.day?.maxtemp_c || null;
-    const futureTempMin = forecastWeather?.forecast?.forecastday?.[2]?.day?.mintemp_c || null;
+    const futureDate = forecastWeather?.forecast?.forecastday?.[2]?.date ?? 0;
+    const futureTempMax = forecastWeather?.forecast?.forecastday?.[2]?.day?.maxtemp_c ?? 0;
+    const futureTempMin = forecastWeather?.forecast?.forecastday?.[2]?.day?.mintemp_c ?? 0;
 
     return (
         <div className="mx-auto w-[80%] mt-4">
@@ -69,7 +69,7 @@ export default async function SSR({searchParams}: SSRprops) {
             <div className={currentWeather ? "mt-4" : "hidden mt-4"}>
                 <h1><span className="font-semibold">{city}</span>, {country}</h1>
                 <h2 className="mt-3">Current conditions:</h2>
-                <div className="mt-2 flex wrap gap-2">
+                <div className="mt-2 flex sm:flex-row gap-2 flex-col">
                     <Cards text={`${currentTemperature}°`} description={`Feels like ${feelsLike}°`}/>
                     <Cards text={
                         <div className="flex items-center gap-2">
@@ -87,7 +87,7 @@ export default async function SSR({searchParams}: SSRprops) {
                 </div>
                 <div className={forecastWeather ? "mt-4" : "hidden mt-4"}>
                     <h2 className="mt-3">Weather forecast:</h2>
-                    <div className="mt-2 flex wrap gap-2">
+                    <div className="mt-2 flex sm:flex-row gap-2 flex-col">
                         <Cards text={`Tomorrow, ${tomorrowDate}`} description={`${tomorrowTempMax}° / ${tomorrowTempMin}° `}/>
                         <Cards text={`${futureDate}`} description={`${futureTempMax}° / ${futureTempMin}° `}/>
                     </div>

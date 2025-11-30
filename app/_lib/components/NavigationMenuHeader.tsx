07c4d33e852f44f304d662f2528b11928d641791
vasenkom <m.vasenko@yandex.ru>
2025-11-30 14:35:52 +0100

    fix: fix header layout

--- app/_lib/components/NavigationMenuHeader.tsx
+++ app/_lib/components/NavigationMenuHeader.tsx
@@ -11,30 +11,29 @@ import Link from "next/link"
 
 export default function NavigationMenuHeader() {
   return (
-    <div className="border-b-1 flex flex-row justify-center h-10">
-        <NavigationMenu>
-            <NavigationMenuList>
-                <div className="flex flex-row justify-between gap-25">
+    <div className="border-b h-10">
+      <div className="w-[80%] mx-auto flex items-center justify-between">
+            
+            {/* LEFT */}
+            <Link href="/">Home</Link>
+
+            {/* RIGHT */}
+            <NavigationMenu>
+                <NavigationMenuList>
                     <NavigationMenuItem>
                         <NavigationMenuLink asChild>
-                            <Link href="/">Home</Link>
+                        <Link href="/CSR">CSR</Link>
                         </NavigationMenuLink>
                     </NavigationMenuItem>
-                    <div className="flex flex-row gap-5">
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild>
-                                <Link href="/CSR">CSR</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                        <NavigationMenuItem>
-                            <NavigationMenuLink asChild>
-                                <Link href="/SSR">SSR</Link>
-                            </NavigationMenuLink>
-                        </NavigationMenuItem>
-                    </div>
-                </div>
-            </NavigationMenuList>
-        </NavigationMenu>
+                    <NavigationMenuItem className="ml-10">
+                        <NavigationMenuLink asChild>
+                        <Link href="/SSR">SSR</Link>
+                        </NavigationMenuLink>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
+            
+      </div>
     </div>
   )
 }
